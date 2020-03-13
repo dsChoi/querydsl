b2a7c6c62ca2df089a5e22a196bebb9822c92f29
choids <setin82@ticketlink.co.kr>
2020-03-13 15:37:47 +0900

    init

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,12 @@
+package study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class QuerydslApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
