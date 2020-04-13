db8fcc9776cf6937f6a7abbeba4ca326e4019e61
choids <setin82@ticketlink.co.kr>
2020-04-13 10:15:01 +0900

    오류 수정.

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -12,7 +12,6 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertion
 
 @SpringBootTest
 @Transactional
